7d91749e2daa3f2eb5b5d17b616c632553ad6973
bvrevanth <revanth47.2017@gmail.com>
2023-03-13 22:43:45 +0530

    added search functionality

---
+++ src/components/CommentsContainer.js
@@ -0,0 +1,18 @@
+import React from "react";
+const commentsData = [
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum doren gyujnh sit amet, constructer adip",
+    replies: [],
+  },
+];
+
+const CommentsContainer = () => {
+  return (
+    <div className="m-5 p-2">
+      <h1 className="text-2xl font-bold">Comments:</h1>
+    </div>
+  );
+};
+
+export default CommentsContainer;
