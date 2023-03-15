7498a74d10cd3ad57b21ff7083be7379075a6460
bvrevanth <revanth47.2017@gmail.com>
2023-03-15 15:47:08 +0530

    added input and send button in live chat section

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
 import { generateRandomName } from "../utils/helper";
@@ -6,6 +6,7 @@ import ChatMessage from "./ChatMessage";
 import { makeRandomMessage } from "../utils/helper";
 
 const LiveChat = () => {
+  const [liveMessage, setLiveMessage] = useState("");
   const dispatch = useDispatch();
 
   const chatMessages = useSelector((store) => store.chat.messages);
@@ -19,17 +20,42 @@ const LiveChat = () => {
           message: makeRandomMessage(20) + " 🚀",
         })
       );
-    }, 500);
+    }, 1500);
 
     return () => clearInterval(i);
   }, []);
 
   return (
-    <div className=" w-full h-[450px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-hidden overflow-y-scroll flex flex-col-reverse">
-      {chatMessages.map((c, i) => (
-        <ChatMessage key={i} name={c.name} message={c.message} />
-      ))}
-    </div>
+    <>
+      <div className=" w-full h-[450px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-hidden overflow-y-scroll flex flex-col-reverse">
+        {chatMessages.map((c, i) => (
+          <ChatMessage key={i} name={c.name} message={c.message} />
+        ))}
+      </div>
+      <form
+        className="w-full p-2 ml-2 border border-black"
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(
+            addMessage({
+              name: "Revanth venkat",
+              message: liveMessage,
+            })
+          );
+          setLiveMessage("");
+        }}
+      >
+        <input
+          className="w-80 px-2 "
+          type="text"
+          value={liveMessage}
+          onChange={(e) => {
+            setLiveMessage(e.target.value);
+          }}
+        />
+        <button className="px-2 mx-2 bg-green-100">Send</button>
+      </form>
+    </>
   );
 };
 
